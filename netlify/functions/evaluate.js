ec8b0af08e95fb8730cb1d5683a76f9c21da0c8b
ifeellikechiefkeefin2009-lang <i.feel.likechiefkeefin2009@gmail.com>
2026-08-22 10:17:51 -0400

    Update evaluate.js

--- netlify/functions/evaluate.js
+++ netlify/functions/evaluate.js
@@ -1,1 +1,41 @@
+exports.handler = async function (event) {
+  if (event.httpMethod !== "POST") {
+    return {
+      statusCode: 405,
+      body: JSON.stringify({
+        error: "Only POST requests are allowed."
+      })
+    };
+  }
 
+  try {
+    const data = JSON.parse(event.body);
+
+    const response = data.response;
+
+    if (!response) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          error: "No AI response was provided."
+        })
+      };
+    }
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "Evalix received the response!",
+        response: response
+      })
+    };
+
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: "Something went wrong."
+      })
+    };
+  }
+};
